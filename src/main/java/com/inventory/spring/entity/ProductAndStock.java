ac8c02b2219b7b5599862ea02c31665aae03a7dd
Ambika-k <105354654+Ambika-k@users.noreply.github.com>
2022-09-25 17:32:44 +0530

    Update ProductAndStock.java

--- src/main/java/com/inventory/spring/entity/ProductAndStock.java
+++ src/main/java/com/inventory/spring/entity/ProductAndStock.java
@@ -1,5 +1,8 @@
 package com.inventory.spring.entity;
-
+/*
+*This is an interface which is used 
+*to encapsulate productName and Stock
+*into single object*/
 public interface ProductAndStock {
 	public String getName();
 	public int getStock();
