441ef2a28ab1210bea83d727dbb605f9016f3c6d
Ambika-k <105354654+Ambika-k@users.noreply.github.com>
2022-09-25 17:31:26 +0530

    Update ProductAndExpiryDate.java

--- src/main/java/com/inventory/spring/entity/ProductAndExpiryDate.java
+++ src/main/java/com/inventory/spring/entity/ProductAndExpiryDate.java
@@ -1,5 +1,9 @@
 package com.inventory.spring.entity;
-
+/*
+*This is an interface which is used to
+*encapsulate productName and ExpiryDate
+*into single object
+*/
 public interface ProductAndExpiryDate {
 	public String getName();
 	public String getExp();
